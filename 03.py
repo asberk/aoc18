72e409e96663576096d62bfe08f7f802cfc674c5
Aaron Berk (grimpoteuthis) <aberk@math.ubc.ca>
2018-12-02 21:54:23 -0800

    day 3 complete

--- 03.py
+++ 03.py
@@ -21,19 +21,21 @@ class Claim:
         self.x1 = int(x1)
         self.y1 = int(y1)
         self.dx, self.dy = int(dx), int(dy)
-        self.x2, self.y2 = self.x1 + self.dx -1, self.y1 + self.dy -1
+        self.x2, self.y2 = self.x1 + self.dx - 1, self.y1 + self.dy - 1
 
     def area(self):
         return self.dx * self.dy
 
     def has_overlap(self, claim):
-        qx1 = (self.x1 <= claim.x1) and (self.x2 >= claim.x1)
-        rx1 = (claim.x1 <= self.x1) and (claim.x2 >= self.x1)
-        qy1 = (self.y1 <= claim.y1) and (self.y2 >= claim.y1)
-        ry1 = (claim.y1 <= self.y1) and (claim.y2 >= self.y1)
-        return (qx1 and qy1) or (rx1 and ry1) or (qx1 and ry1) or (rx1 and qy1)
+        qx = (self.x1 <= claim.x1) and (self.x2 >= claim.x1)
+        rx = (claim.x1 <= self.x1) and (claim.x2 >= self.x1)
+        qy = (self.y1 <= claim.y1) and (self.y2 >= claim.y1)
+        ry = (claim.y1 <= self.y1) and (claim.y2 >= self.y1)
+        return (qx and qy) or (rx and ry) or (qx and ry) or (rx and qy)
 
     def get_overlap_area(self, claim):
+        if not self.has_overlap(claim):
+            return 0
         max_x1 = np.maximum(self.x1, claim.x1)
         min_x2 = np.minimum(self.x2, claim.x2)
         max_y1 = np.maximum(self.y1, claim.y1)
@@ -42,21 +44,29 @@ class Claim:
         
 claims = [Claim(s) for s in x]
 
-overlaps = {}
-for i, p in enumerate(claims):
-    for j, q in enumerate(claims[i+1:]):
-        if p.has_overlap(q):
-            overlaps[(i, j)] = p.get_overlap_area(q)
+arr = np.zeros((1000, 1000))
 
-p1 = np.sum([v for v in overlaps.values()])
+for i, c in enumerate(claims):
+    for xi in range(c.x1, c.x2 + 1):
+        for yi in range(c.y1, c.y2 + 1):
+            arr[xi, yi] += 1
+p1 = (arr > 1).sum()
 print(p1)
-
-wrong_answers = [176468, 191479, ]
 
 print('\nPart 2: ', end='', flush=True)
 
-test = None
-# x = test
-
-p2 = None
-print(p2)
+possibles = []
+olap_mat = np.zeros((len(claims), len(claims)))
+for i, p in enumerate(claims):
+    for j, q in enumerate(claims[i+1:], i+1):
+        olap_mat[i,j] = p.has_overlap(q)
+        olap_mat[j,i] = olap_mat[i,j]
+    if not olap_mat[i].any():
+        possibles.append(p.ID)
+        
+if len(possibles) == 1:
+    p2 = possibles[0]
+    print(p2)
+else:
+    print(possibles)
+    print('something went very wrong')
